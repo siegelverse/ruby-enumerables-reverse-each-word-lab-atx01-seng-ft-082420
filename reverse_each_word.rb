66ff23605a43a2d230623432251d513e2e99e641
Marcus Siegel <msiegel37@gmail.com>
2020-08-13 01:31:01 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -6,3 +6,12 @@ def reverse_each_word(string)
   end
   return_array.join(" ")
 end
+
+def reverse_each_word(string)
+ original_array = string.split(" ")
+  return_array = []
+  original_array.collect do|string|
+    return_array << string.reverse
+  end
+  return_array.join(" ")
+end
